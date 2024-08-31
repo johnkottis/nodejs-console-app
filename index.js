0a501df470b5ef65689ac2488cc285c29f75f22a
John Kottis <johnkottis@gmail.com>
2024-08-31 11:26:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
-	var	txtController = require('./controllers/txtController');
-	txtController.wordCount();
+const txtController = require('./controllers/txtController');
+
+(async () => {
+    try {
+        await txtController.wordCount(process.argv[2]);
+    } catch (err) {
+        console.error("Error executing word count:", err);
+    }
+})();
